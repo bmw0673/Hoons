e53ef22fe65011c2f12c3876fd20204191563d1d
훈즈 <Administrator@304-10>
2023-11-17 17:55:15 +0900

    Hoons 초안

---
+++ src/main/java/com/hoons/HoonsApplication.java
@@ -0,0 +1,13 @@
+package com.hoons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoons/HoonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
