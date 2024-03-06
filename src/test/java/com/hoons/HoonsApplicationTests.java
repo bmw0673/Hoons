4b84abd3ee6c7525c2c5a094775a7f536d5a1e45
choihome <choihome@192.168.219.112>
2024-03-06 18:19:51 +0900

    2024.03.06 18:19

--- src/test/java/com/hoons/HoonsApplicationTests.java
+++ src/test/java/com/hoons/HoonsApplicationTests.java
@@ -1,12 +1,10 @@
 package com.hoons;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HoonsApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
